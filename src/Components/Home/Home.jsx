594edcf172b512a3c7876b9c7aae1dfc6ada1e59
Sahil Abrar <zeroxkhan2@gmail.com>
2023-09-15 09:57:31 +0600

    added toasts and restricted minus values in the remaining section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import { FaBookOpen } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 const Home = () => {
   const [courses, setCources] = useState([]);
@@ -12,89 +11,106 @@ const Home = () => {
   const [remainingCources, setRemainingCources] = useState(20);
   const [totalCreditCost, setTotalCreditCost] = useState(0);
 
-
   useEffect(() => {
-    fetch('data.json').then(res => res.json()).then(data => setCources(data))
-  }, [])
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setCources(data));
+  }, []);
 
+  const handleCourses = course => {
+    const ifAvailable = selectCourses.find(items => items.id === course.id);
 
-  const handleCourses = (course) => {
-    const ifAvailable = selectCourses.find(items => items.id === course.id)
-
-    let count = course.credit_hr;
+    let credits = course.credit_hr;
 
     if (ifAvailable) {
-      return toast.error('Oops! Cannot add more!', {
-    position: toast.POSITION.TOP_CENTER
-});
-    }
-    else {
-      selectCourses.forEach((item) => {
-        count = count + item.credit_hr
-      })
+      return toast.error('💀 Oops! Cannot add more!', {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    } else {
+      selectCourses.forEach(item => {
+        credits = credits + item.credit_hr;
+      });
 
-      const totalCourcesRemaining = 20 - count;
-      setTotalCreditCost(count);
-      setRemainingCources(totalCourcesRemaining);
-      setSelectCourses([...selectCourses, course]);
+      const totalCourcesRemaining = 20 - credits;
+      if (credits > 20) {
+        return toast('😥 Oops! No More credits available!', {
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        });
+      } else {
+        setTotalCreditCost(credits);
+        setRemainingCources(totalCourcesRemaining);
+        setSelectCourses([...selectCourses, course]);
+      }
     }
-  }
+  };
 
   return (
     <div className="">
-      
-      <div className='text-3xl font-bold'>
+      <div className="text-3xl font-bold">
         <h1>Course Registration</h1>
       </div>
-      <div className='flex gap-5 mx-auto'>
+      <div className="flex gap-5 mx-auto">
         <div className="mt-5 grid grid-cols-3 mx-auto gap-5">
-
-          {
-          courses.map(course => (
-            <div key={
-                course.id
-              }
-              class="card h-auto shadow-xl">
+          {courses.map(course => (
+            <div key={course.id} class="card h-auto shadow-xl">
               <figure class="px-10 pt-10">
-                <img src={
-                    course.course_img
-                  }
-                  alt=""
-                  class="rounded-xl"/>
+                <img src={course.course_img} alt="" class="rounded-xl" />
               </figure>
               <div class="card-body items-center text-center">
-                <h2 class="card-title text-base font-bold">
-                  {
-                  course.title
-                }</h2>
-                <h1 className='text-[gray]'>
-                  {
-                  course.description
-                  }</h1>
-                <div className='flex gap-3 items-center'>
-                  <p className='text-base text-[gray] font-medium'>$ Price : {course.price}</p>
+                <h2 class="card-title text-base font-bold">{course.title}</h2>
+                <h1 className="text-[gray]">{course.description}</h1>
+                <div className="flex gap-3 items-center">
+                  <p className="text-base text-[gray] font-medium">
+                    $ Price : {course.price}
+                  </p>
                   <FaBookOpen></FaBookOpen>
-                  <p className='items-center text-base text-[gray] font-medium'>Credit : {course.credit_hr}hr</p>
+                  <p className="items-center text-base text-[gray] font-medium">
+                    Credit : {course.credit_hr}hr
+                  </p>
                 </div>
                 <div class="card-actions">
-                  <button onClick={() => handleCourses(course)} className='rounded-lg mt-5 bg-[#2F80ED] px-[100px] py-[5px] text-white font-semibold'>Select</button>
-                  <ToastContainer/>
+                  <button
+                    onClick={() => handleCourses(course)}
+                    className="rounded-lg mt-5 bg-[#2F80ED] px-[100px] py-[5px] text-white font-semibold"
+                  >
+                    Select
+                  </button>
+                  <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                  />
                 </div>
               </div>
             </div>
-          ))
-        } </div>
-        <Cart selectCourses={selectCourses} remainingCources={remainingCources} totalCreditCost={totalCreditCost}></Cart>
+          ))}{' '}
+        </div>
+        <Cart
+          selectCourses={selectCourses}
+          remainingCources={remainingCources}
+          totalCreditCost={totalCreditCost}
+        ></Cart>
       </div>
     </div>
-
-
   );
 };
 
 Home.prototypes = {
-  home: PropTypes.isRequired
-
-}
+  home: PropTypes.isRequired,
+};
 
 export default Home;
